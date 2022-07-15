524b145306487061e8d304542948cc9a09e0aa05
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-15 16:17:34 -0300

    add comentarios

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
 
-
-
 let novoNumero = true;
 let operador;
 let numeroAnterior;
 
+//atualizar display/tela
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
         display.textContent = texto.toLocaleString('BR');
@@ -19,12 +18,15 @@ const atualizarDisplay = (texto) => {
     }
 }
 
+//inserir numero 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 
 numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
 
+//conferir se possui operacoes pendentes
 const operacaoPendente = () => operador !== undefined;
 
+//calculos: soma, subtracao, multiplicacao e divisao
 const calcular = () => {
     if (operacaoPendente()) {
         const numeroAtual = parseFloat(display.textContent.replace(',','.'))
@@ -34,13 +36,16 @@ const calcular = () => {
     }
 }
 
+//igual
 const ativarIgual = () => {
     calcular();
     operador = undefined;
 }
 
+//limapr display
 const limparDisplay = () => display.textContent = ''
 
+//limpar todo calculo
 const limparCalculo = () => {
     limparDisplay();
     operador = undefined;
@@ -48,17 +53,22 @@ const limparCalculo = () => {
     numeroAnterior = undefined;
 }
 
+//remove ultimo numero do display
 const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1)
 
+//inverte sinal de + ou - ao ser clicado
 const inverterSinal = () => {
     novoNumero = true;
     atualizarDisplay(display.textContent * -1)
 }
 
+//confere se possui alguma virgula no display
 const existeDecimal = () => display.textContent.indexOf(',') !== -1; 
 
+//confere se existe algo digitado no display
 const existeValor = () => display.textContent.length > 0;
 
+//insere a virgula
 const inserirDecimal = () => {
     if (!existeDecimal()) {
         if (existeValor()) {
@@ -69,6 +79,7 @@ const inserirDecimal = () => {
     }
 }
 
+//pegando ids e ouvindo click
 document.getElementById("igual").addEventListener('click', ativarIgual);
 document.getElementById("limparDisplay").addEventListener('click', limparDisplay);
 document.getElementById("limparCalculo").addEventListener('click', limparCalculo);
@@ -76,6 +87,7 @@ document.getElementById("backspace").addEventListener('click', removerUltimoNume
 document.getElementById("inverter").addEventListener('click', inverterSinal);
 document.getElementById("decimal").addEventListener('click', inserirDecimal);
 
+//seleciona operador
 const selecionarOperador = (evento) => {
     if (!novoNumero) {
         calcular();
@@ -84,7 +96,6 @@ const selecionarOperador = (evento) => {
         numeroAnterior = parseFloat(display.textContent.replace(',','.'));
     }
 }
-
 
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
 
