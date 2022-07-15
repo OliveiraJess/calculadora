1d2ba19932788cadac32f4fa11b016686cc8ea7c
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-15 15:46:35 -0300

    atualizar display, limpar display, limpar calculo e inserir virgula

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=tecla]');
 const operadores = document.querySelectorAll('[id*=operador]');
 
+
+
 let novoNumero = true;
+let operador;
+let numeroAnterior;
 
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
@@ -19,9 +23,66 @@ const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 
 numeros.forEach(numero => numero.addEventListener('click', inserirNumero));
 
+const operacaoPendente = () => operador !== undefined;
 
-const selecionarOperador = () => {
+const calcular = () => {
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent)
+        novoNumero = true;
+        const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`)
+        atualizarDisplay(resultado)
+    }
+}
+
+const ativarIgual = () => {
+    calcular();
+    operador = undefined;
+}
+
+const limparDisplay = () => display.textContent = ''
+
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
     novoNumero = true;
+    numeroAnterior = undefined;
+}
+
+const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1)
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1)
+}
+
+const existeDecimal = () => display.textContent.indexOf(',') !== -1; 
+
+const existeValor = () => display.textContent.length > 0;
+
+const inserirDecimal = () => {
+    if (!existeDecimal()) {
+        if (existeValor()) {
+            atualizarDisplay(",");
+        } else {
+            atualizarDisplay("0,");
+        }
+    }
+}
+
+document.getElementById("igual").addEventListener('click', ativarIgual);
+document.getElementById("limparDisplay").addEventListener('click', limparDisplay);
+document.getElementById("limparCalculo").addEventListener('click', limparCalculo);
+document.getElementById("backspace").addEventListener('click', removerUltimoNumero);
+document.getElementById("inverter").addEventListener('click', inverterSinal);
+document.getElementById("decimal").addEventListener('click', inserirDecimal);
+
+const selecionarOperador = (evento) => {
+    if (!novoNumero) {
+        calcular();
+        novoNumero = true;
+        operador = evento.target.textContent;
+        numeroAnterior = parseFloat(display.textContent);
+    }
 }
 
 
