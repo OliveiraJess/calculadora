f3c24ab3c9983df794999a0e554a12eb865b8867
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-15 16:11:18 -0300

    pagando click das teclas para a aparecer no display

--- main.js
+++ main.js
@@ -87,3 +87,35 @@ const selecionarOperador = (evento) => {
 
 
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
+
+//pegando click no teclado para aparecer no display
+const mapaTeclado = {
+    0: 'tecla0',
+    1: 'tecla1',
+    2: 'tecla2',
+    3: 'tecla3',
+    4: 'tecla4',
+    5: 'tecla5',
+    6: 'tecla6',
+    7: 'tecla7',
+    8: 'tecla8',
+    9: 'tecla9',
+    '/': 'operadorDividir',
+    '*': 'operadorMultiplicar',
+    '-': 'operadorSubtrair',
+    '+': 'operadorAdicionar',
+    '=': 'igual',
+    Enter: 'igual',
+    Backspace: 'backspace',
+    c: 'limparDisplay',
+    Escape: 'limparCalculo',
+    ',': 'decimal',
+}
+
+const mapearTeclado = (evento) => {
+    const tecla = evento.key;
+    const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+};
+
+document.addEventListener('keydown', mapearTeclado);
